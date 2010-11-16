891519b46ce87432092c0a873114c9502a0b0bbf
Aaron Mills <aaron@jailbreakcreative.com>
2010-11-15 23:54:59 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Microblog::Application.config.secret_token = 'd0e4bdb3e46deb0e6371cadaf6bffc0d7e81bd5946afa31773939cab190d3fd5b4cd8c2fc1bf77ebc08d86588afff35de3a5c538cb89b8ec6d56ad17c8ad4b1e'
